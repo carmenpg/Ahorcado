f8393b0e808f5363e2b9f2b5a4c717fc9dd7081e
Carmen Perez Gomez <carmenperezgomez@MacBook-Pro-de-Carmen.local>
2024-12-16 14:41:25 +0100

    proyecto creado

---
+++ src/main/java/ahorcado/Ahorcado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ahorcado;
+
+/**
+ *
+ * @author carmenperezgomez
+ */
+public class Ahorcado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
